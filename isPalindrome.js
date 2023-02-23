a7780a07240495aa4cc0da4ecb65cd791fe67c92
River Hansen <riverbh01@gmail.com>
2023-02-23 10:05:04 -0700

    first commit of challenge-is-palindrome

---
+++ isPalindrome.js
@@ -0,0 +1,13 @@
+// Write your code below
+
+function palindrome(str) {
+    str = str.replace(/(\W)|(\_)/g,"").split("");
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] !== str[str.length - 1 -i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(palindrome("tacocat"))
